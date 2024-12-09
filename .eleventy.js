929084d1dbe6aa7d748764f88642f79db6afdab6
ebeshero <ebbondar@gmail.com>
2024-12-08 23:55:55 -0500

    adding three.js page to 11ty model

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function (eleventyConfig){
     eleventyConfig.addPassthroughCopy("_src/_includes/");
     eleventyConfig.addPassthroughCopy("./_src/css/");
+    eleventyConfig.addPassthroughCopy("./_src/main.js");
     eleventyConfig.addPassthroughCopy("./_src/img/");
+    eleventyConfig.addPassthroughCopy("./_src/threed-images/");
     eleventyConfig.addNunjucksFilter('getTopicKeys', require('./_src/parsekeys.js').getTopicKeys);
 
     return{
---
+++ _src/three.md
@@ -0,0 +1,13 @@
+---
+title: Three
+param: DIGIT
+layout: base.njk
+permalink: three.html
+---
+
+
+<canvas id="bg"></canvas>
+   <section class="text">
+     <h1>Spinning Objects in the Penn State Behrend Library</h1>
+   </section>
+    <script type="module" src="main.js"></script>
